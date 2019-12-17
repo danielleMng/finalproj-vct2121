3de463d92fc2aa723e95ac5c871ef83350485246
danielle Manguele <danielemanguele@gmail.com>
2019-12-16 21:58:17 -0600

    fixed animations

--- js/app.js
+++ js/app.js
@@ -20,12 +20,34 @@ $(document).ready(function(){
     })
     .setClassToggle('#slideshow','fade-in')
     .addTo(controller)
+
+
     var controller1= new ScrollMagic.Controller();
     var scene1= new ScrollMagic.Scene({
       triggerElement:'.contact-section'
     })
     .setClassToggle('.contact-section','fade-in')
     .addTo(controller1)
+
+
+    var tl = new TimelineMax();
+    tl.from("#img1", 1.5, {autoAlpha: 0, scale: 1},"-=1");
+    tl.from("#img2", 1.5, {autoAlpha: 0, scale: 1},"-=1");
+    tl.from("#img3", 1.5, {autoAlpha: 0, scale: 1},"-=1");
+    tl.from("#img4", 1.5, {autoAlpha: 0, scale: 1},"-=1");
+    tl.from("#img5", 1.5, {autoAlpha: 0, scale: 1},"-=1");
+    tl.from("#img6", 1.5, {autoAlpha: 0, scale: 1},"-=1");
+    var controller3 = new ScrollMagic.Controller()
+    const scene3 = new ScrollMagic.Scene({
+      triggerElement: "#myportfolio",
+      triggerHook: 'onLeave',
+      duration: "100%",
+      offset:-200
+    })
+    .setPin('#myportfolio')
+    .setTween(tl)
+    .addTo(controller)
+
 
     
 })
